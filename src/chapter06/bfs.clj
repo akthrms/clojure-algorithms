60feb2815dabe91b012a79b74ab139fa1bb7e3c0
akthrms <akt.hrms@gmail.com>
2020-11-21 13:26:38 +0900

    update

---
+++ src/chapter06/bfs.clj
@@ -0,0 +1,33 @@
+(ns chapter06.bfs)
+
+(def graph {:you    '(:alice :bob :claire)
+            :bob    '(:anuj :peggy)
+            :alice  '(:peggy)
+            :claire '(:thom :jonny)
+            :anuj   '()
+            :peggy  '()
+            :thom   '()
+            :jonny  '()})
+
+(defn seller? [person-name]
+  (->> person-name
+       (name)
+       (seq)
+       (last)
+       (= \m)))
+
+(defn search [person-name]
+  (loop [[person & persons :as search-queue] (get graph person-name)
+         searched '()]
+    (and (< 0 (count search-queue))
+         (cond
+           (.contains searched person) (recur persons
+                                              searched)
+           (seller? person) (do (println (name person) "is a mango seller!")
+                                true)
+           :else (recur (concat persons (get graph person))
+                        (cons person searched))))))
+
+(search :you)
+; thom is a mango seller!
+; => true
